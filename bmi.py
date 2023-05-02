582df77f204cc726eae6bd5634e787a56f814429
Jun Kei <teo.junkei@hotmail.com>
2023-05-02 23:30:51 +0800

    Stuff

---
+++ bmi.py
@@ -0,0 +1,9 @@
+def main():
+    print("ET0735 (DEVOps for AIoT) - Lab 2 - Introduction to Python")
+    def display_main_menu():
+        print("dispay_main_menu")
+    def calc_average():
+        print("calc_average")
+
+
+
