ee06990119e6eca3c09b7cf7557209218a1e0eaf
Jun Kei <teo.junkei@hotmail.com>
2023-05-04 11:04:13 +0800

    update version

--- bmi.py
+++ bmi.py
@@ -1,9 +1,16 @@
-def main():
-    print("ET0735 (DEVOps for AIoT) - Lab 2 - Introduction to Python")
-    def display_main_menu():
-        print("dispay_main_menu")
-    def calc_average():
-        print("calc_average")
-
-
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi=weight/height**2
+    print(bmi)
+    if bmi<18.5:
+        x = -1
+        print(x)
+    elif 18.5<=bmi<=25.0:
+        y = 0
+        print(y)
+    elif bmi>=25.0:
+        z = 1
+        print(z)
+calculate_bmi(weight=57, height=1.73)
 
